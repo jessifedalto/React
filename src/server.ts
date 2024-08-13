9ee96e03a17a637cd1bbdf259d0ebdc1b6c7fea2
jessifedalto <jessicafedalto.jf@gmail.com>
2024-08-13 11:13:35 -0300

    feat: test done

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,26 @@
-console.log("Rodando!");
+import fastify from 'fastify'
 
-let a: string;
+const server = fastify()
+
+server.listen({port: 3333}).then(() => 
+    console.log("Server Running")    
+)
+
+server.get('/', () => {
+    return "Olá mundo!"
+});
+
+server.get('/home', () => {
+    return "Olá usuário";
+});
+// server.get('/ping', async (request, reply) => {
+//   return 'pong\n'
+// })
+
+// server.listen({ port: 8080 }, (err, address) => {
+//   if (err) {
+//     console.error(err)
+//     process.exit(1)
+//   }
+//   console.log(`Server listening at ${address}`)
+// })
